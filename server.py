4dceefc2146cd5f385b34fde7b96bbff5ac869f4
Gareth Daunton <gdaunton@gmail.com>
2019-03-31 23:30:59 -0400

    Init commit

---
+++ server.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_restful import Resource, Api
+from textgenrnn import textgenrnn
+
+app = Flask(__name__)
+api = Api(app)
+
+class ShowerThoughts(Resource):
+  def post(self):
+    output = self.textgen.generate(1, temperature=0.5, return_as_list=True, progress=False)
+    return { 'response_type': 'in_channel', 'attachments':  [{ 'text': output[0] }] }
+
+  def __init__(self):
+    self.textgen = textgenrnn(weights_path='showerthoughts_weights.hdf5', vocab_path='showerthoughts_vocab.json', config_path='showerthoughts_config.json')
+
+api.add_resource(ShowerThoughts, '/')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
