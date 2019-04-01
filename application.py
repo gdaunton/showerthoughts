a3640c6c5588583ee7edbba560c65cc6e1664ec4
Gareth Daunton <gdaunton@gmail.com>
2019-04-01 02:30:05 -0400

    Add threading.

--- application.py
+++ application.py
@@ -2,24 +2,35 @@ from flask import Flask
 import tensorflow as tf
 from flask_restful import Resource, Api
 from textgenrnn import textgenrnn
+import threading
 
 application = Flask(__name__)
 api = Api(application)
 
-global textgen
+global textgen, output
 textgen = textgenrnn(weights_path='showerthoughts_weights.hdf5', vocab_path='showerthoughts_vocab.json', config_path='showerthoughts_config.json')
-textgen.generate()
+output = textgen.generate(5, temperature=0.5, return_as_list=True, progress=False)
+
+def calcNewItems():
+  global output
+  print('generating new thoughts')
+  output += textgen.generate(5, temperature=0.5, return_as_list=True, progress=False)
 
 class ShowerThoughts(Resource):
   def get(self):
     return 'Hello world'
 
   def post(self):
-    output = textgen.generate(1, temperature=0.5, return_as_list=True, progress=False)
-    return { 'response_type': 'in_channel', 'attachments':  [{ 'text': output[0] }] }
+    global output
+    if(len(output) <= 1):
+      thr = threading.Thread(target=calcNewItems)
+      thr.start()
+    if(len(output) >= 2):
+      return { 'response_type': 'in_channel', 'attachments':  [{ 'text': output.pop() }] }
+    return { 'response_type': 'in_channel', 'attachments':  [{ 'text': 'try again' }] }
 
 api.add_resource(ShowerThoughts, '/')
 
 if __name__ == '__main__':
   application.debug = True
-  application.run()
+  application.run(port=12345)
